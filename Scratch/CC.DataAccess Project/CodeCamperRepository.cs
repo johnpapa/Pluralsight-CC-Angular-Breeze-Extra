04abb05a5cbdd827eef481d39de3bcd332bedc2e
John Papa <john@johnpapa.net>
2013-09-08 13:43:43 -0400

    Initial push for all of the 'from scratch' files

---
+++ Scratch/CC.DataAccess Project/CodeCamperRepository.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using Breeze.WebApi;
+using Newtonsoft.Json.Linq;
+
+namespace CodeCamper.DataAccess
+{
+    /// <summary>
+    /// Repository (a "Unit of Work" really) of CodeCamper models.
+    /// </summary>
+    public class CodeCamperRepository
+    {
+        private readonly EFContextProvider<CodeCamperDbContext>
+            _contextProvider = new EFContextProvider<CodeCamperDbContext>();
+
+        private CodeCamperDbContext Context { get { return _contextProvider.Context; } }
+
+        public string Metadata
+        {
+            get { return _contextProvider.Metadata(); }
+        }
+
+        public SaveResult SaveChanges(JObject saveBundle)
+        {
+            return _contextProvider.SaveChanges(saveBundle);
+        }
+
+        public IQueryable<Session> Sessions
+        {
+            get { return Context.Sessions; }
+        }
+
+        public IQueryable<Person> Speakers
+        {
+            get { return Context.Persons.Where(p => p.SpeakerSessions.Any()); }
+        }
+
+        public IQueryable<Person> Persons
+        {
+            get { return Context.Persons; }
+        }
+
+        public IQueryable<Room> Rooms
+        {
+            get { return Context.Rooms; }
+        }
+        public IQueryable<TimeSlot> TimeSlots
+        {
+            get { return Context.TimeSlots; }
+        }
+        public IQueryable<Track> Tracks
+        {
+            get { return Context.Tracks; }
+        }
+    }
+}
---
+++ Scratch/CC.Web Assets/Controllers/BreezeController.cs
@@ -0,0 +1,72 @@
+using System.Linq;
+using System.Web.Http;
+using CodeCamper;
+using CodeCamper.DataAccess;
+using Newtonsoft.Json.Linq;
+using Breeze.WebApi;
+
+namespace CCJS_Server.Controllers
+{
+    [BreezeController]
+    public class BreezeController : ApiController
+    {
+        // Todo: inject via an interface rather than "new" the concrete class
+        readonly CodeCamperRepository _repository = new CodeCamperRepository();
+
+        [HttpGet]
+        public string Metadata()
+        {
+            return _repository.Metadata;
+        }
+
+        [HttpPost]
+        public SaveResult SaveChanges(JObject saveBundle)
+        {
+            return _repository.SaveChanges(saveBundle);
+        }
+
+        [HttpGet]
+        public IQueryable<Session> Sessions()
+        {
+            return _repository.Sessions;
+        }
+
+        [HttpGet]
+        public IQueryable<Person> Speakers()
+        {
+            return _repository.Speakers;
+        }
+
+        [HttpGet]
+        public IQueryable<Person> Persons()
+        {
+            return _repository.Persons;
+        }
+
+
+        /// <summary>
+        /// Query returing a 1-element array with a lookups object whose 
+        /// properties are all Rooms, Tracks, and TimeSlots.
+        /// </summary>
+        /// <returns>
+        /// Returns one object, not an IQueryable, 
+        /// whose properties are "rooms", "tracks", "timeslots".
+        /// The items arrive as arrays.
+        /// </returns>
+        [HttpGet]
+        public object Lookups()
+        {
+            var rooms = _repository.Rooms;
+            var tracks = _repository.Tracks;
+            var timeslots = _repository.TimeSlots;
+            return new { rooms, tracks, timeslots };
+        }
+
+        // Diagnostic
+        [HttpGet]
+        public string Ping()
+        {
+            return "pong";
+        }
+    }
+}
